48c4f195c07b4754b086b8f52150cb73973f1d3e
Nurym Alibekov <nurymw.a@gmail.com>
2025-01-16 20:29:52 +0500

    minor changes

--- main.go
+++ main.go
@@ -51,47 +51,46 @@ func main() {
 
 	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
 
-		if r.Method == http.MethodGet {
-
+		if r.Method != http.MethodPost {
 			http.ServeFile(w, r, "web/templates/register.html")
-			
-		} else if r.Method == http.MethodPost {
+			return
+		} 
+		
 
-			email := strings.TrimSpace(r.FormValue("email"))
-			password := strings.TrimSpace(r.FormValue("password"))
-			if email == "" || password == "" {
-				http.Error(w, "Email and password cannot be empty", http.StatusBadRequest)
-				return
-			}
-
-			var exists bool
-			err := DB.QueryRow("SELECT EXISTS (SELECT 1 FROM users WHERE email = $1)", email).Scan(&exists)
-			if err != nil {
-				log.Printf("Error checking email existence: %v", err)
-				http.Error(w, "Internal server error", http.StatusInternalServerError)
-				return
-			}
-			if exists {
-				http.Error(w, "Email already registered", http.StatusConflict)
-				return
-			}
-			
-
-			hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-			if err != nil {
-				http.Error(w, "Failed to hash password", http.StatusInternalServerError)
-				return
-			}
-
-			_, err = DB.Exec("INSERT INTO users (email, password_hash) VALUES ($1, $2)", email, hashedPassword)
-			if err != nil {
-				log.Printf("Error inserting new user: %v", err)
-				http.Error(w, "Internal server error", http.StatusInternalServerError)
-				return
-			}
-
-			http.Redirect(w, r, "/main", http.StatusSeeOther)
+		email := strings.TrimSpace(r.FormValue("email"))
+		password := strings.TrimSpace(r.FormValue("password"))
+		if email == "" || password == "" {
+			http.Error(w, "Email and password cannot be empty", http.StatusBadRequest)
+			return
 		}
+
+		var exists bool
+		err := DB.QueryRow("SELECT EXISTS (SELECT 1 FROM users WHERE email = $1)", email).Scan(&exists)
+		if err != nil {
+			log.Printf("Error checking email existence: %v", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		if exists {
+			http.Error(w, "Email already registered", http.StatusConflict)
+			return
+		}
+			
+
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+		if err != nil {
+			http.Error(w, "Failed to hash password", http.StatusInternalServerError)
+			return
+		}
+
+		_, err = DB.Exec("INSERT INTO users (email, password_hash) VALUES ($1, $2)", email, hashedPassword)
+		if err != nil {
+			log.Printf("Error inserting new user: %v", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+
+		http.Redirect(w, r, "/main", http.StatusSeeOther)
 	})
 	
 	http.HandleFunc("/main", func(w http.ResponseWriter, r *http.Request) {
