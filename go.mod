5fa3dc43a45afb9279f49600250362eb55210534
Nurym Alibekov <nurymw.a@gmail.com>
2025-01-16 20:04:46 +0500

    register finished

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/NurymGM/hotel
 go 1.22.6
 
 require github.com/lib/pq v1.10.9
+
+require golang.org/x/crypto v0.32.0 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
@@ -12,7 +14,7 @@ func main() {
 	}
 	defer closeDB()
 
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/static")))) // Serve static files (css, img)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/static")))) 
 
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { 
@@ -20,7 +22,48 @@ func main() {
 	})
 
 	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "web/templates/register.html")
+
+		if r.Method == http.MethodGet {
+
+			http.ServeFile(w, r, "web/templates/register.html")
+
+		} else if r.Method == http.MethodPost {
+
+			email := strings.TrimSpace(r.FormValue("email"))
+			password := strings.TrimSpace(r.FormValue("password"))
+			if email == "" || password == "" {
+				http.Error(w, "Email and password cannot be empty", http.StatusBadRequest)
+				return
+			}
+
+			var exists bool
+			err := DB.QueryRow("SELECT EXISTS (SELECT 1 FROM users WHERE email = $1)", email).Scan(&exists)
+			if err != nil {
+				log.Printf("Error checking email existence: %v", err)
+				http.Error(w, "Internal server error", http.StatusInternalServerError)
+				return
+			}
+			if exists {
+				http.Error(w, "Email already registered", http.StatusConflict)
+				return
+			}
+			
+
+			hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+			if err != nil {
+				http.Error(w, "Failed to hash password", http.StatusInternalServerError)
+				return
+			}
+
+			_, err = DB.Exec("INSERT INTO users (email, password_hash) VALUES ($1, $2)", email, hashedPassword)
+			if err != nil {
+				log.Printf("Error inserting new user: %v", err)
+				http.Error(w, "Internal server error", http.StatusInternalServerError)
+				return
+			}
+
+			http.Redirect(w, r, "/main", http.StatusSeeOther)
+		}
 	})
 	
 	http.HandleFunc("/main", func(w http.ResponseWriter, r *http.Request) {
