fb4ab98275f3f2412daafa0cde7eee50d9b82b3d
Nurym Alibekov <nurymw.a@gmail.com>
2025-01-17 16:52:59 +0500

    getting hotels from db and listing them on page

--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"database/sql"
+	"html/template"
 	"log"
 	"net/http"
 	"strings"
 
 	"golang.org/x/crypto/bcrypt"
 )
+
+type Hotel struct {
+	Price       float64
+	Rating      float64
+	IsAvailable bool
+	Image       string
+}
 
 func main() {
 
@@ -16,19 +24,17 @@ func main() {
 	}
 	defer closeDB()
 
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/static")))) 
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/static"))))
 
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
 		if r.Method != http.MethodPost {
 			http.ServeFile(w, r, "web/templates/index.html")
 			return
-		} 
-
+		}
 
 		email := r.FormValue("email")
-    	password := r.FormValue("password")
+		password := r.FormValue("password")
 
 		var hashedPassword string
 		err := DB.QueryRow("SELECT password_hash FROM users WHERE email = $1", email).Scan(&hashedPassword)
@@ -47,15 +53,14 @@ func main() {
 		}
 
 		http.Redirect(w, r, "/main", http.StatusSeeOther)
-	})	
+	})
 
 	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
 
 		if r.Method != http.MethodPost {
 			http.ServeFile(w, r, "web/templates/register.html")
 			return
-		} 
-		
+		}
 
 		email := strings.TrimSpace(r.FormValue("email"))
 		password := strings.TrimSpace(r.FormValue("password"))
@@ -75,7 +80,6 @@ func main() {
 			http.Error(w, "Email already registered", http.StatusConflict)
 			return
 		}
-			
 
 		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 		if err != nil {
@@ -92,11 +96,46 @@ func main() {
 
 		http.Redirect(w, r, "/main", http.StatusSeeOther)
 	})
-	
-	http.HandleFunc("/main", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "web/templates/main.html")
-	})	
 
+	http.HandleFunc("/main", func(w http.ResponseWriter, r *http.Request) {
+
+		// Query hotels from the database
+		rows, err := DB.Query("SELECT price, rating, is_available, img FROM hotels")
+		if err != nil {
+			http.Error(w, "Unable to fetch hotels", http.StatusInternalServerError)
+			log.Println(err)
+			return
+		}
+		defer rows.Close()
+
+		// Collect the hotels
+		var hotels []Hotel
+		for rows.Next() {
+			var h Hotel
+			err := rows.Scan(&h.Price, &h.Rating, &h.IsAvailable, &h.Image)
+			if err != nil {
+				http.Error(w, "Error reading hotel data", http.StatusInternalServerError)
+				log.Println(err)
+				return
+			}
+			hotels = append(hotels, h)
+		}
+
+		// Parse and render the main.html template
+		tmpl, err := template.ParseFiles("web/templates/main.html")
+		if err != nil {
+			http.Error(w, "Error loading template", http.StatusInternalServerError)
+			log.Println(err)
+			return
+		}
+
+		// Pass hotels to the template
+		err = tmpl.Execute(w, hotels)
+		if err != nil {
+			http.Error(w, "Error rendering template", http.StatusInternalServerError)
+			log.Println(err)
+		}
+	})
 
 	addr := ":8080"
 	log.Printf("Server is running on http://localhost%s\n", addr)
